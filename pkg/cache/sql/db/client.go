a14527715950b5ad0579f4d822d1092a26f45b76
Silvio Moioli <silvio@moioli.net>
2024-05-31 14:24:58 +0200

    comments clarified
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- pkg/cache/sql/db/client.go
+++ pkg/cache/sql/db/client.go
@@ -300,7 +300,7 @@ func (c *Client) NewConnection() error {
 		return err
 	}
 
-	// necessary to prevent memory races. Otherwise, all conections will close and in-memory database for fields table
+	// necessary to prevent memory races. Otherwise, all connections will close and in-memory database for fields table
 	// will be closed automatically. Setting max connections also minimizes SQLITE_BUSY errors.
 	// https://github.com/mattn/go-sqlite3/blob/master/README.md#faq
 	sqlDB.SetConnMaxIdleTime(-1)
--- pkg/cache/sql/partition/partition.go
+++ pkg/cache/sql/partition/partition.go
@@ -8,11 +8,17 @@ import (
 	"k8s.io/apimachinery/pkg/util/sets"
 )
 
-// Partition represents filtering of a request's results by namespace or a list of resource names.
-// If All is false and Names is empty, that indicates an empty search space that result in an empty list
+// Partition represents filtering of a request's results
 type Partition struct {
-	Namespace   string
-	All         bool
+	// if true, do not apply any filtering, return all results. Overrides all other fields
 	Passthrough bool
-	Names       sets.Set[string]
+
+	// if non-empty, only resources in the specified namespaces will be returned
+	Namespace string
+
+	// if true, return all results, while still honoring Namespace. Overrides Names
+	All bool
+
+	// if non-empty, only resources with matching names will be returned
+	Names sets.Set[string]
 }
