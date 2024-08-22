87c60553fee16a9d6510413d3ace85867e216a08
Alejandro Ruiz <alejandro.ruiz@suse.com>
2024-08-22 08:43:31 +0200

    feat(metrics): improve lasso_controller_total_cached_object
    * Is updated frequently
    * Can be split by context, by calling `WithContextID` over the
      `context.Context` passed to `Start`
    * Deleted when cache is stopped

---
+++ pkg/cache/metrics.go
@@ -0,0 +1,56 @@
+package cache
+
+import (
+	"context"
+	"time"
+
+	"github.com/rancher/lasso/pkg/metrics"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+const (
+	cacheMetricsCollectionPeriod = 1 * time.Minute
+)
+
+func (f *sharedCacheFactory) collectMetrics() sharedCacheFactoryMetrics {
+	f.lock.RLock()
+	defer f.lock.RUnlock()
+
+	gvks := make(map[schema.GroupVersionKind]int)
+	for gvk, c := range f.caches {
+		items := c.GetStore().List()
+		gvks[gvk] = len(items)
+	}
+	return sharedCacheFactoryMetrics{
+		gvks: gvks,
+	}
+}
+
+type sharedCacheFactoryMetrics struct {
+	// gvks is the total count of cache items by GroupVersionKind
+	gvks map[schema.GroupVersionKind]int
+}
+
+func (f *sharedCacheFactory) startMetricsCollection(ctx context.Context) {
+	go func() {
+		contextID := metrics.ContextID(ctx)
+		timer := time.NewTimer(cacheMetricsCollectionPeriod)
+		defer timer.Stop()
+		for {
+			factoryMetrics := f.collectMetrics()
+			for gvk, count := range factoryMetrics.gvks {
+				metrics.IncTotalCachedObjects(contextID, gvk.Group, gvk.Version, gvk.Kind, float64(count))
+			}
+
+			timer.Reset(cacheMetricsCollectionPeriod)
+			select {
+			case <-ctx.Done():
+				for gvk := range factoryMetrics.gvks {
+					metrics.DelTotalCachedObjects(contextID, gvk.Group, gvk.Version, gvk.Kind)
+				}
+				return
+			case <-timer.C:
+			}
+		}
+	}()
+}
