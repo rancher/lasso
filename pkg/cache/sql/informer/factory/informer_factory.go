42de963b1efa61e1219117334b9da2a715af7830
Silvio Moioli <silvio@moioli.net>
2024-08-30 17:07:43 +0200

    sql: allow multiple informers to start in parallel
    Creating an Informer used to block creation of any other Informers
    for the whole duration of the warm-up time via one mutex.
    
    This patch allows for multiple Informers to be created in parallel,
    with one mutex per gvk.
    
    One overall RWMutex is still kept to avoid races with Reset().
    
    Signed-off-by: Silvio Moioli <silvio@moioli.net>

--- pkg/cache/sql/informer/factory/informer_factory.go
+++ pkg/cache/sql/informer/factory/informer_factory.go
@@ -1,5 +1,5 @@
 /*
-Package factory provides an cache factory for the sql-based cache.
+Package factory provides a cache factory for the sql-based cache.
 */
 package factory
 
@@ -7,12 +7,13 @@ import (
 	"fmt"
 	"os"
 	"sync"
-
-	"github.com/rancher/lasso/pkg/cache/sql/informer"
+	"time"
 
 	"github.com/rancher/lasso/pkg/cache/sql/db"
 	"github.com/rancher/lasso/pkg/cache/sql/encryption"
+	"github.com/rancher/lasso/pkg/cache/sql/informer"
 	sqlStore "github.com/rancher/lasso/pkg/cache/sql/store"
+	"github.com/rancher/lasso/pkg/log"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/dynamic"
@@ -25,15 +26,16 @@ const EncryptAllEnvVar = "CATTLE_ENCRYPT_CACHE_ALL"
 
 // CacheFactory builds Informer instances and keeps a cache of instances it created
 type CacheFactory struct {
-	informerCreateLock sync.RWMutex
-	wg                 wait.Group
-	dbClient           DBClient
-	stopCh             chan struct{}
-	encryptAll         bool
+	wg          wait.Group
+	dbClient    DBClient
+	stopCh      chan struct{}
+	stopChMutex sync.RWMutex
+	encryptAll  bool
 
 	newInformer newInformer
 
-	cache map[schema.GroupVersionKind]*informer.Informer
+	informers       sync.Map
+	informerMutexes sync.Map
 }
 
 type newInformer func(client dynamic.ResourceInterface, fields [][]string, transform cache.TransformFunc, gvk schema.GroupVersionKind, db sqlStore.DBClient, shouldEncrypt bool, namespace bool) (*informer.Informer, error)
@@ -72,7 +74,6 @@ func NewCacheFactory() (*CacheFactory, error) {
 	return &CacheFactory{
 		wg:          wait.Group{},
 		stopCh:      make(chan struct{}),
-		cache:       map[schema.GroupVersionKind]*informer.Informer{},
 		encryptAll:  os.Getenv(EncryptAllEnvVar) == "true",
 		dbClient:    dbClient,
 		newInformer: informer.NewInformer,
@@ -82,13 +83,28 @@ func NewCacheFactory() (*CacheFactory, error) {
 // CacheFor returns an informer for given GVK, using sql store indexed with fields, using the specified client. For virtual fields, they must be added by the transform function
 // and specified by fields to be used for later fields.
 func (f *CacheFactory) CacheFor(fields [][]string, transform cache.TransformFunc, client dynamic.ResourceInterface, gvk schema.GroupVersionKind, namespaced bool) (Cache, error) {
-	result, ok := f.getCacheIfExists(gvk)
+	// first of all block Reset() until we are done
+	f.stopChMutex.RLock()
+	defer f.stopChMutex.RUnlock()
+
+	// then, block other concurrent calls to CacheFor for the same gvk
+	rmutex, _ := f.informerMutexes.LoadOrStore(gvk, &sync.Mutex{})
+	mutex := rmutex.(*sync.Mutex)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	// then, if we have a cached informer, return it right away
+	result, ok := f.informers.Load(gvk)
 	if ok {
-		return Cache{ByOptionsLister: result}, nil
+		return Cache{ByOptionsLister: result.(*informer.Informer)}, nil
 	}
 
-	f.informerCreateLock.Lock()
-	defer f.informerCreateLock.Unlock()
+	// otherwise, create a new informer...
+	start := time.Now()
+	log.Infof("CacheFor STARTS creating informer for %v", gvk)
+	defer func() {
+		log.Infof("CacheFor ISDONE creating informer for %v (took %v)", gvk, time.Now().Sub(start))
+	}()
 
 	_, encryptResourceAlways := defaultEncryptedResourceTypes[gvk]
 	shouldEncrypt := f.encryptAll || encryptResourceAlways
@@ -97,27 +113,16 @@ func (f *CacheFactory) CacheFor(fields [][]string, transform cache.TransformFunc
 		return Cache{}, err
 	}
 
-	if f.cache == nil {
-		f.cache = make(map[schema.GroupVersionKind]*informer.Informer)
-	}
-	f.cache[gvk] = i
+	// ...store it in the informers cache...
+	f.informers.Store(gvk, i)
+
 	f.wg.StartWithChannel(f.stopCh, i.Run)
 	if !cache.WaitForCacheSync(f.stopCh, i.HasSynced) {
 		return Cache{}, fmt.Errorf("failed to sync SQLite Informer cache for GVK %v", gvk)
 	}
 
+	// ...and return it
 	return Cache{ByOptionsLister: i}, nil
-}
-
-func (f *CacheFactory) getCacheIfExists(gvk schema.GroupVersionKind) (*informer.Informer, bool) {
-	f.informerCreateLock.RLock()
-	defer f.informerCreateLock.RUnlock()
-
-	result, ok := f.cache[gvk]
-	if ok {
-		return result, true
-	}
-	return nil, false
 }
 
 // Reset closes the stopCh which stops any running informers, assigns a new stopCh, resets the GVK-informer cache, and resets
@@ -128,12 +133,25 @@ func (f *CacheFactory) Reset() error {
 		return nil
 	}
 
-	f.informerCreateLock.Lock()
-	defer f.informerCreateLock.Unlock()
+	// first of all wait until all CacheFor() calls that create new informers are finished. Also block any new ones
+	f.stopChMutex.Lock()
+	defer f.stopChMutex.Unlock()
 
+	// now that we are alone, stop all informers created until this point
 	close(f.stopCh)
 	f.stopCh = make(chan struct{})
-	f.cache = make(map[schema.GroupVersionKind]*informer.Informer)
+
+	// and get rid of all references to those informers and their mutexes
+	f.informers.Range(func(key, value any) bool {
+		f.informers.Delete(key)
+		return true
+	})
+	f.informerMutexes.Range(func(key, value any) bool {
+		f.informerMutexes.Delete(key)
+		return true
+	})
+
+	// finally, reset the DB connection
 	err := f.dbClient.NewConnection()
 	if err != nil {
 		return err
